3942476adb1abf33b7e4e561764a71961e693d70
Ivan Stiazkin <ivan@nesusvet.com>
2018-03-04 10:40:35 +0300

    Benchmark on Leo Tolstoy

---
+++ examples/leo.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import logging
+import sys
+import time
+
+from cipher import VigenereCipher
+
+u"""
+Можно скачать Войну и Мир здесь
+    http://vojnaimir.ru/files/book1.txt
+    http://vojnaimir.ru/files/book2.txt
+"""
+
+RUSSIAN = u'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+
+def timer(func):
+    logger = logging.getLogger('timer.%s' % func.__name__)
+    def timed(*args, **kwargs):
+        started_at = time.time()
+        result = func(*args, **kwargs)
+        finished_at = time.time()
+        logger.debug('Function %s run for %.3f sec', func.__name__, finished_at - started_at)
+        return result
+
+    return timed
+
+
+def from_file(filename, **kwargs):
+    with codecs.open(filename, 'r', **kwargs) as file_obj:
+        for line in file_obj:
+            yield line
+
+
+@timer
+def main(abc, key, input, output):
+    cipher = VigenereCipher(key, abc)
+    for line in lines:
+        encoded = cipher.encode(line)
+        output.write(encoded)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print """Use as python leo.py book1.txt cp1251 пароль > my_encoded.txt"""
+        exit(1)
+
+    logging.basicConfig(level=logging.DEBUG)
+    _, filename, encoding, key = sys.argv
+
+    lines = [line.encode('utf') for line in from_file(filename, encoding=encoding)]
+
+    main(RUSSIAN, key, lines, sys.stdout)
