04de08ca86e685a257c315df59daf942a4e20905
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-03-09 22:16:59 +0200

    Connect

--- examples/slackbot.py
+++ examples/slackbot.py
@@ -9,6 +9,14 @@ def main(slack_token):
     slack_client = SlackClient(slack_token)
     print 'Initialized'
 
+    # Connect to Slack API
+    # https://slackapi.github.io/python-slackclient/
+    is_connected = slack_client.rtm_connect(with_team_state=False)
+    if not is_connected:
+        print 'Failed to connect to slack'
+        return 1
+    print 'Connected'
+
 
 if __name__ == '__main__':
     slack_token = os.environ.get('SLACK_TOKEN', '<your-token-here>')
