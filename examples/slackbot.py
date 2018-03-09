5c994575681d9204801f8e29ed03d59e15430c51
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-03-09 22:09:38 +0200

    Read environment var SLACK_TOKEN

--- examples/slackbot.py
+++ examples/slackbot.py
@@ -1,7 +1,10 @@
+import os
 
-def main():
-    print 'Yay!'
+
+def main(slack_token):
+    print 'Yay!', slack_token
 
 
 if __name__ == '__main__':
-    exit(main())
+    slack_token = os.environ.get('SLACK_TOKEN', '<your-token-here>')
+    exit(main(slack_token))
