b987adbd4f9839108bc3c2f98198bd6bdb08fabe
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-05-22 22:51:05 +0300

    POST handler with simple validation of a required field

--- project/app.py
+++ project/app.py
@@ -13,5 +13,14 @@ def get(slug):
     return 'nothing to show for {}'.format(slug), 404
 
 
+@app.route('/', methods=['POST'])
+def post():
+    form = flask.request.form
+    if 'url' not in form:
+        flask.abort(400, 'url parameter is required')
+
+    return form['url']
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
