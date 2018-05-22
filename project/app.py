55755f044e7f5dbeaf362c3dcdf5500e74368c1f
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-05-22 22:39:20 +0300

    Stub GET handler

--- project/app.py
+++ project/app.py
@@ -8,5 +8,10 @@ def index():
     return 'Hello, world'
 
 
+@app.route('/<slug>')
+def get(slug):
+    return 'nothing to show for {}'.format(slug), 404
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
