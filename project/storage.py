82c5296ba8f4855a322c3c0f81b4c3316a1b136d
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-05-22 23:12:52 +0300

    Adds bootstraped storage module

---
+++ project/storage.py
@@ -0,0 +1,15 @@
+
+
+def find(link):
+    """Returns an id if this link already was saved or None if not"""
+    return
+
+
+def get(number):
+    """Takes id of the link and returns original link as text"""
+    return
+
+
+def put(link):
+    """Saves link to storage and returns id assigned"""
+    return 0
