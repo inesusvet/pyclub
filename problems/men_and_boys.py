5ceac13fa965a7988ed717305b9db73b4855769d
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-08-20 11:17:35 +0300

    Added Men & boys problem

---
+++ problems/men_and_boys.py
@@ -0,0 +1,22 @@
+"""
+See https://www.codewars.com/kata/sort-out-the-men-from-boys-1/train/python
+"""
+
+
+def men_from_boys(numbers_list):
+  """
+  >>> men_from_boys([7,3,14,17])
+  [14, 17, 7, 3]
+  >>> men_from_boys([2,43,95,90,37])
+  [2, 90, 95, 43, 37]
+  >>> men_from_boys([20,33,50,34,43,46])
+  [20, 34, 46, 50, 43, 33]
+  """
+  return []
+
+
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
+
