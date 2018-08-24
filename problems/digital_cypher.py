d6b355057be67a3076c000c03962b5c7653d1039
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-08-24 12:13:15 +0300

    Added new problem about cypher

---
+++ problems/digital_cypher.py
@@ -0,0 +1,18 @@
+"""
+See https://www.codewars.com/kata/digital-cypher
+"""
+
+
+def encode(text, key):
+  """
+  >>> encode("scout", 1939)
+  [20, 12, 18, 30, 21]
+  >>> encode("masterpiece", 1939)
+  [14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]
+  """
+  return []
+
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
