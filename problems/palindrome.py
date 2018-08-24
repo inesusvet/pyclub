28210feb2e65a8f7a3f08b32f649a90e720996f4
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-08-24 12:14:45 +0300

    Added palindrome task

---
+++ problems/palindrome.py
@@ -0,0 +1,25 @@
+"""
+See http://www.codewars.com/kata/palindrome-strings
+
+A palindrome is a word, phrase, number, or other sequence of characters which
+reads the same backward or forward. This includes capital letters, punctuation,
+and word dividers.
+"""
+
+def is_palindrome(string):
+    """
+    >>> is_palindrome('anna')
+    True
+    >>> is_palindrome('Walter')
+    False
+    >>> is_palindrome(12321)
+    True
+    >>> is_palindrome(123456)
+    False
+    """
+    return
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
