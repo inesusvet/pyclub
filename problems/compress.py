2e4409c92cba88db7cc4af77ea06efac4b075fae
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-09-06 14:32:33 +0300

    Added task of string compression

---
+++ problems/compress.py
@@ -0,0 +1,22 @@
+
+def compress_string(text):
+    """
+    Написать фукнцию, которая будет "сжимать" переданную строку.
+    Вместо повтора символов будет указывать сколько раз символ повторялся.
+
+    >>> compress_string('aaabbbbcddd')
+    'a3b4cd3'
+    >>> compress_string('abcdefgh')
+    'abcdefgh'
+    >>> compress_string('aaaaabaaaaa')
+    'a5ba5'
+    >>> compress_string('')
+    ''
+    """
+    return
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
