ce842e06750a29dc0022155a265bc2aa4022a7b4
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2018-12-24 15:24:39 +0300

    Added a problem of Keyboard Layout Switch

---
+++ problems/switch.py
@@ -0,0 +1,8 @@
+def switch(text):
+    """
+    >>> switch('ghbdtn')
+    'привет'
+    >>> switch('C]tim tot 'nb[ vzurb[ ahfywepcrb[ ,ekjr lf dsgtq xf.')
+    'Съешь еще этих мягких французских булок да выпей чаю'
+    """
+    return ''
