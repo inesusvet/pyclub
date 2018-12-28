c16af79d4e53cd42fcf48800c8ac375830fa95e7
Ivan Styazhkin <ivan@nesusvet.com>
2018-12-28 18:48:05 +0300

    Fix quotes

--- problems/switch.py
+++ problems/switch.py
@@ -2,7 +2,7 @@ def switch(text):
     """
     >>> switch('ghbdtn')
     'привет'
-    >>> switch('C]tim tot 'nb[ vzurb[ ahfywepcrb[ ,ekjr lf dsgtq xf.')
+    >>> switch("C]tim tot 'nb[ vzurb[ ahfywepcrb[ ,ekjr lf dsgtq xf.")
     'Съешь еще этих мягких французских булок да выпей чаю'
     """
     return ''
