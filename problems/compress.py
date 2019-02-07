ad41a8fecb7d1ebfd156d06be300243dd5e6edac
Ivan Styazhkin <ivan@nesusvet.com>
2019-02-07 23:58:20 +0300

    Add extra function and translate the task

--- problems/compress.py
+++ problems/compress.py
@@ -1,9 +1,8 @@
-#! -*- coding: utf-8 -*-
 
 def compress_string(text):
     """
-    Написать фукнцию, которая будет "сжимать" переданную строку.
-    Вместо повтора символов будет указывать сколько раз символ повторялся.
+    Write a function which will "compress" input string.
+    Instead of repeating a char there should be just number of repetitions.
 
     >>> compress_string('aaabbbbcddd')
     'a3b4cd3'
@@ -11,10 +10,28 @@ def compress_string(text):
     'abcdefgh'
     >>> compress_string('aaaaabaaaaa')
     'a5ba5'
+    >>> compress_string('aaaaaaaaaab')
+    'a10b'
     >>> compress_string('')
     ''
     """
     return
+
+
+def decompress_string(text):
+    """
+    Write a inverse function.
+    
+    >>> decompress_string('')
+    ''
+    >>> decompress_string('a10b')
+    'aaaaaaaaaab'
+    >>> decompress_string('a5ba5')
+    'aaaaabaaaaa'
+    >>> decompress_string('a3b4cd3')
+    'aaabbbbcddd'
+    """
+    return 
 
 
 if __name__ == '__main__':
