4f1d08bc4e32712cdeb0e45b33f7502f9c33a4d4
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2019-02-09 21:40:05 +0300

    Added split-by task with no description

---
+++ problems/split_by.py
@@ -0,0 +1,13 @@
+import json
+
+
+def split_by(sequence, chunk_size=10):
+    """
+    >>> split_by([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+    [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]
+    >>> split_by([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)
+    [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10]]
+    >>> split_by('abcdefghijklmnopq', 4)
+    ['abcd', 'efgh', 'ijkl', 'mnop', 'q']
+    """
+    return []
