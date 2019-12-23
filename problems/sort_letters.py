ab4e4757dc1705075b2b31eec63d39576e41fc16
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2019-12-23 16:24:36 +0200

    Add a task on sorting letters in a string

---
+++ problems/sort_letters.py
@@ -0,0 +1,13 @@
+def sort_letters(text):
+    """
+    >>> sort_letters('aaabccccdeefffff')
+    'fffffccccaaaeebd'
+    >>> sort_letters('abcdefghijklmnop')
+    'abcdefghijklmnop'
+    >>> sort_letters('')
+    ''
+    >>> sort_letters('aba')
+    'aab'
+    >>> sort_letters('abcabccba')
+    'aaabbbccc'
+    """
