36feadb74fb08e579f94d2ff6541587a5b006686
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-05-14 18:05:29 +0300

    Added K-th largest problem

---
+++ problems/kth_largest.py
@@ -0,0 +1,13 @@
+def kth_largest(numbers, k):
+    """
+    See https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+    >>> kth_largest([3, 2, 1, 5, 6, 4], 2)
+    5
+    >>> kth_largest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+    4
+    >>> kth_largest([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+    9
+    >>> kth_largest([10, 9, 5, 6, 4, 7, 2, 1, 3, 8], 3)
+    8
+    """
