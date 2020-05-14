d319c1645062d5340702419e4b395605ee7f27ed
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-05-14 18:11:53 +0300

    Added Longest increasing Subsequence problem

---
+++ problems/lis.py
@@ -0,0 +1,13 @@
+def lis_len(nums):
+    """
+    See https://leetcode.com/problems/longest-increasing-subsequence/
+
+    >>> lis_len([])
+    0
+    >>> lis_len([1, 2, 3])
+    3
+    >>> lis_len([1, 0, 1])
+    2
+    >>> lis_len([10, 9, 2, 5, 3, 7, 101, 18])
+    4
+    """
