d6ad0a901ecafb18d63dfad6bd8f1b4a70cca68a
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-05-14 18:09:36 +0300

    Added Merge Intervals problem

---
+++ problems/merge_intervals.py
@@ -0,0 +1,9 @@
+def merge(intervals):
+    """
+    See https://leetcode.com/problems/merge-intervals/
+
+    >>> merge([[1, 3], [2, 6], [8, 10], [15, 18]])
+    [[1, 6], [8, 10], [15, 18]]
+    >>> merge([[1, 4], [4, 5]])
+    [[1, 5]]
+    """
