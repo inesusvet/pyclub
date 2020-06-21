a9f5da87adbd1061bb2853e81f90322ad5849865
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-06-21 12:06:56 +0300

    Added a problem of infinite digits sequence

---
+++ problems/digit_sequence.py
@@ -0,0 +1,19 @@
+def main(n):
+    """
+    Imagine all natural numbers written one by one without a delimiter.
+    > 012345678910111213141516171819202122..
+
+    Your task is to write a function which answers the question:
+    > Which digit is placed by a given position n?
+
+    >>> main(0)
+    0
+    >>> main(7)
+    7
+    >>> main(11)
+    0
+    >>> main(16)
+    1
+    >>> main(17)
+    3
+    """
