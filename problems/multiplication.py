fa66da8faa7ca14ef30c80c52153ff14c916aa24
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-06-24 22:59:51 +0300

    Added the Multiplication problem

---
+++ problems/multiplication.py
@@ -0,0 +1,11 @@
+def main(numbers):
+    """
+    Given a sequence of integers. Build a new sequence of the same length. Each element of a new
+    sequence should be calculated as a multiplication of elements of the original sequence except
+    the element with the same index.  Think about possible corner cases.
+
+    >>> main([1, 5, 3])
+    [15, 3, 5]
+    >>> main([1, 2, 2, 5, 8])
+    [160, 80, 80, 32, 20]
+    """
