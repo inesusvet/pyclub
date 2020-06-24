ac82808e93268a1a5281dd32b6a8a7c092c7bbc3
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-06-24 22:21:41 +0300

    Added the Single Element problem

---
+++ problems/single_element.py
@@ -0,0 +1,20 @@
+def main(numbers):
+    """
+    See https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+    You are given a sorted array consisting of only integers where every element appears exactly
+    twice, except for one element which appears exactly once. Find this single element that appears
+    only once.
+
+    Follow up: Your solution should run in O(log n) time and O(1) space.
+
+    >>> main([1, 1, 2, 2, 3, 3, 4, 4, 8, 8])
+    2
+    >>> main([3, 3, 7, 7, 10, 11, 11])
+    10
+    >>> main([])
+    >>> main([1])
+    1
+    >>> main([1, 1, 2])
+    2
+    """
