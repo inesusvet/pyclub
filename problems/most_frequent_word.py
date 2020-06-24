556415be91872f16aedac9d4a928723d45d390ba
Ivan Styazhkin <ivan.styazhkin@datarobot.com>
2020-06-24 23:07:19 +0300

    Added the Most Frequent Word problem

---
+++ problems/most_frequent_word.py
@@ -0,0 +1,7 @@
+def most_frequent_word(text):
+    """
+    >>> most_frequent_word('i think therefore i am')
+    'i'
+    >>> most_frequent_word("be the change that you wish to see in the world")
+    'the'
+    """
