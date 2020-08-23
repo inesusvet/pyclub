fc75de35c0c74fb66f04549fa2e9e95635edd36b
Ivan Stiazkin <ivan@nesusvet.com>
2020-08-23 18:06:33 +0300

    Added description to letters sort problem
    In case you are interested, you may read about https://en.wikipedia.org/wiki/1997%E2%80%9398_Bradford_City_A.F.C._season

--- problems/sort_letters.py
+++ problems/sort_letters.py
@@ -1,5 +1,9 @@
 def sort_letters(text):
     """
+    Return a string of the same letters where all the letters are sorted by
+    its frequency in descending order. In case of equal frequency letters
+    should go in the order they were met in the original string.
+
     >>> sort_letters('aaabccccdeefffff')
     'fffffccccaaaeebd'
     >>> sort_letters('abcdefghijklmnop')
@@ -11,3 +15,4 @@ def sort_letters(text):
     >>> sort_letters('abcabccba')
     'aaabbbccc'
     """
+    return ''
