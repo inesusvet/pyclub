af138803ddd3a135d9c131ad4609fc4c736e5140
Ivan Stiazkin <ivan@nesusvet.com>
2020-08-25 12:44:42 +0300

    Added two sorting problems
    In case you are interested, you may read about https://en.wikipedia.org/wiki/Hanzala_Badghisi

---
+++ problems/counter_sort.py
@@ -0,0 +1,12 @@
+def counter_sort(numbers):
+    """
+    >>> counter_sort([2, 1, 4, 3, 6, 5])
+    [1, 2, 3, 4, 5, 6]
+    >>> counter_sort([10, 15, 13, 12, 11, 14])
+    [10, 11, 12, 13, 14, 15]
+    >>> counter_sort([0, -10, 1, 2, 3, -5])
+    [-10, -5, 0, 1, 2, 3]
+    >>> counter_sort([])
+    []
+    """
+    return []
---
+++ problems/k_merge.py
@@ -0,0 +1,17 @@
+def k_merge(*args):
+    """
+    Given an arbitrary number of sorted lists, build a merged sorted list.
+
+    >>> k_merge()
+    >>> k_merge([])
+    []
+    >>> k_merge([1], [1])
+    [1, 1]
+    >>> k_merge([1, 3], [5], [2, 4])
+    [1, 2, 3, 4, 5]
+    >>> k_merge([1, 1, 1], [3], [2, 2])
+    [1, 1, 1, 2, 2, 3]
+    >>> k_merge([1,4,5], [1,3,4], [2,6])
+    [1, 1, 2, 3, 4, 4, 5, 6]
+    """
+    return []
