ef7f1fdf20d77032421023ef78bbe5624f96726b
Ivan Stiazkin <ivan@nesusvet.com>
2021-03-05 16:16:33 +0200

    Added the last one task
    In case you are interested, you may read about https://en.wikipedia.org/wiki/Mary_Clifford

---
+++ problems/last_one.py
@@ -0,0 +1,16 @@
+def main(numbers):
+    """
+    Given a list of zeroes and ones. Find index of the last one.
+    When there is no ones in the sequence, return -1
+
+    >>> main([1])
+    0
+    >>> main([0, 0, 0, 1])
+    3
+    >>> main([1, 1, 0, 0])
+    1
+    >>> main([1, 0, 0, 1])
+    3
+    >>> main([0, 0, 0, 0])
+    -1
+    """
