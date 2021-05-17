71f52011d6952e78329c2e85932e3341f8681ac4
Ivan Stiazkin <ivan@nesusvet.com>
2021-05-17 11:14:59 +0300

    Added a task for checking possible palindrome
    In case you are interested, you may read about https://en.wikipedia.org/wiki/Mission_Dolores,_San_Francisco

---
+++ problems/maybe_palindrome.py
@@ -0,0 +1,14 @@
+def main(text: str) -> bool:
+    """
+    Implement a function which will check if it is possible to build a valid
+    palindrome from a sequence of letters. Only permutations are allowed.
+
+    >>> main("anna")
+    True
+    >>> main("nana")
+    True
+    >>> main("nanam")
+    True
+    >>> main("dogo")
+    False
+    """
