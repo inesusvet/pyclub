65d51130a54457b97b27806cc26d8ff9cdcf7cb0
Ivan Styazhkin <ivan@nesusvet.com>
2022-07-15 11:43:24 +0100

    Add a problem about increasing sequence

---
+++ problems/increasing_sequence.py
@@ -0,0 +1,13 @@
+def main(numbers: List[int]) -> bool:
+    """
+    We need a function which checks a sequence of numbers.
+    It should return True only when every next number is greater
+    than the previous one.
+
+    >>> main([1, 2, 3])
+    True
+    >>> main([1, 3, 2])
+    False
+    >>> main([2, 3, 1])
+    False
+    """
