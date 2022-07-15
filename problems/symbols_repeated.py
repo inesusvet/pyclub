2450834fd87af9b3813d858cd394b2d1dce5117a
Ivan Styazhkin <ivan@nesusvet.com>
2022-07-15 11:49:24 +0100

    Add a problem of symbol repetition

---
+++ problems/symbols_repeated.py
@@ -0,0 +1,12 @@
+def main(s1: str, s2: str) -> int:
+    """
+    Count how many times each symbol from the first string
+    occurs in the second string. Return a sum of all repetitions
+
+    >>> main("a", "foobar")
+    1
+    >>> main("ab", "foobar")
+    2
+    >>> main("ab", "abba")
+    4
+    """
